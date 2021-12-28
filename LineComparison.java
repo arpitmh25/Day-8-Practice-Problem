924db69f7e4267939815f26013ae1e4e3042cb2b
arpitmh25 <95077786+arpitmh25@users.noreply.github.com>
2021-12-28 22:16:26 +0530

    Update LineComparison.java
    Implement Changes

--- LineComparison.java
+++ LineComparison.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 class LineLengthComparison {
     public void lineComparisonUc1() {
 
@@ -33,13 +34,13 @@ class LineLengthComparison {
         int y4 = t.nextInt();
         double length2 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
         System.out.println("lengthof line" + length2);
-        if (length1==(length2))
+        if (length1 == (length2))
             System.out.println("Two Lines Are Equal");
         else
             System.out.println("Two Line Are Not Equal");
     }
 
-    public void lineComparisonUc3(){
+    public void lineComparisonUc3() {
         Scanner t = new Scanner(System.in);
         System.out.println("Enter the Co-ordinates of 1st line ");
         System.out.println("Enter the Co-ordinates of x1 and y1 : ");
@@ -48,7 +49,7 @@ class LineLengthComparison {
         System.out.println("Enter the Co-ordinates of x2 and y2 : ");
         int x2 = t.nextInt();
         int y2 = t.nextInt();
-        Double length1 = Math.sqrt((x2 - x1)^2  +  (y2 - y1)^2);
+        Double length1 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
         System.out.println();
         System.out.println("Enter the Co-ordinates of 2nd line ");
         System.out.println("Enter the Co-ordinates of x3 and y3 : ");
@@ -57,11 +58,11 @@ class LineLengthComparison {
         System.out.println("Enter the Co-ordinates of x4 and y4 : ");
         int x4 = t.nextInt();
         int y4 = t.nextInt();
-        Double length2 = Math.sqrt((x4 - x3)^2  +  (y4 - y3)^2);
-        System.out.println("Length of the first Line is : " + length1 + " units" );
-        System.out.println("Length of the second Line is : " + length2 + " units" );
+        Double length2 = Math.sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
+        System.out.println("Length of the first Line is : " + length1 + " units");
+        System.out.println("Length of the second Line is : " + length2 + " units");
         System.out.println();
-        if(length1.compareTo(length2) == 1)
+        if (length1.compareTo(length2) == 1)
             System.out.println("Length of Line 1 is greater than Line 2 ");
         else if (length1.compareTo(length2) == -1)
             System.out.println("Length of Line 1 is less than Line 2 ");
@@ -70,10 +71,10 @@ class LineLengthComparison {
 
     }
 
-		public static void main(String[] args) {
-			LineComparison measure = new LineComparison();
-                measure.lineComparisonUc1();
-                measure.lineComparisonUc2();
-                measure.lineComparisonUc3();
+    public static void main(String[] args) {
+        LineLengthComparison measure = new LineLengthComparison();
+        measure.lineComparisonUc1();
+        measure.lineComparisonUc2();
+        measure.lineComparisonUc3();
     }
 }
